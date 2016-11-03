28e2feaffb3f73f1bf73e22ac2814db9292534be
fgarciareyna <facu.garcia@gmail.com>
2016-11-03 20:34:08 -0300

    Agregado el parser, funcionando ok

--- TSB/src/interfaz/Principal.java
+++ TSB/src/interfaz/Principal.java
@@ -1,8 +1,10 @@
 package interfaz;
 
+import soporte.Parser;
+
 
 public class Principal {
     public static void main(String[] args) {
-        
+        Parser p = new Parser ("¡Hola!, ¿Cómo va?.\n¿Esto es una loñcura!!?");
     }
 }
